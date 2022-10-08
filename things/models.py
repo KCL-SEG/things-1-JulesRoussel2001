f6d296a0516e6c9411036b22de9d123478a4405b
Jules Roussel <k20074057@kcl.ac.uk>
2022-10-08 17:21:09 +0100

    Add validation on name of a Thing

--- things/models.py
+++ things/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 class Thing(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(
+    max_length=30,
+    unique=True,
+    blank=False
+    )
     description = models.TextField()
     quantity = models.IntegerField()
